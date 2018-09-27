ad1b5b574cb60a5772f3284e22149820b44bcee4
lEGENDARYskyE <43337911+lEGENDARYskyE@users.noreply.github.com>
2018-09-27 18:53:24 +0300

    First commit
    Ensimmäinen testi

---
+++ KahviKone/src/juomaAutomaatti.java
@@ -0,0 +1,82 @@
+import java.math.*;
+public class juomaAutomaatti {
+							// @author Santeri Kilpeläinen
+ private int teetä;
+ private int kahvia;
+ private int kaakaota;
+ 
+ 
+ 	public juomaAutomaatti() {
+	this.teetä = 50;
+	this.kahvia = 50;
+	this.kaakaota = 50;
+ 	}
+
+
+		// GETTERIT
+		public int getTeetä() {
+			return teetä;
+		}		
+		public int getKahvia() {
+			return kahvia;
+		}		
+		public int getKaakaota() {
+			return kaakaota;
+		}
+		
+		// SETTERIT
+		public void setTeetä(int teetä) {
+			this.teetä = teetä;
+		}
+
+		public void setKahvia(int kahvia) {
+			this.kahvia = kahvia;
+		}
+
+		public void setKaakaota(int kaakaota) {
+			this.kaakaota = kaakaota;
+		}
+		
+		// Teen valmistus
+		public void valmistaTee() {
+			if (this.teetä -10 < 0) {
+				this.teetä = 0;
+				System.out.println("Täytä säiliö, ei teetä sinulle :)");
+				return ;}
+				
+				this.teetä -= 10;
+				System.out.println("Teenne on valmis. Teetä jäljellä " + this.teetä + " yksikköä.");
+				}
+		// Kahvin valmistus
+		public void valmistaKahvi() {
+			if (this.kahvia -10 < 0 ) {
+				this.kahvia = 0;
+				System.out.println("Täytä säiliö, ei kahvia sinulle :)");
+				return; }
+			
+				this.kahvia -= 10;
+				System.out.println("Kahvinne on valmis. Kahvia jäljellä " + this.kahvia + " yksikköä.");
+				}
+		// Kaakaon valmistus
+		public void valmistaKaakao() {
+			if (this.kaakaota -10 < 0) {
+				this.kaakaota = 0;
+				System.out.println("Täytä säiliö, ei kaakaota sinulle :)");
+				return; }
+				
+				this.kaakaota  -= 10; 
+				System.out.println("Kaakaonne on valmis. Kaakaota jäljellä " + this.kaakaota + " yksikköä.");
+				}
+
+		//Tilttaus  
+		public boolean onnistuuko() {
+			int luku = (int)(Math.random() * 100+1);
+			return false;
+		}
+		
+		// toString()
+		@Override
+		public String toString() {
+			return "JuomaAutomaatti: [teetä=" + teetä + ", kahvia=" + kahvia + ", kaakaota=" + kaakaota + "]";
+		}
+}
---
+++ KahviKone/src/kahviSetti.java
@@ -0,0 +1,68 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+public class kahviSetti {
+	
+	
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+				
+		// juomaAutomaatin luonti
+		juomaAutomaatti omaKone = new juomaAutomaatti();
+		
+		// Scannerin luonti
+		Scanner tilaus = new Scanner(System.in);
+
+		
+		// Tekstipohjainen UI ja looppaus
+		while(true) {
+		
+		// Joka tilauksen alussa arpoo numeron joka päättää tilauksen toimimisen
+		int random = (int)(Math.random() * 100+1);
+		
+		// UI
+		System.out.println("***Juoma-Automaatti***\n");
+		System.out.println("1. Kahvi\n2. Tee\n3. Kaakao\n4. Sammuta kone"
+				+ "\n*****************");
+		int order = tilaus.nextInt();
+		
+		// Tilausten suoritus
+		if (order == 1 && random > 25) {
+			System.out.println("Odota hetki, kahvianne valmistetaan");
+			Thread.sleep(1500);
+			omaKone.valmistaKahvi();
+			System.out.println(omaKone);
+			}
+		
+		else if (order == 2 && random > 25) {
+			System.out.println("Odota hetki, teetänne valmistetaan");
+			Thread.sleep(1500);
+			omaKone.valmistaTee();
+			System.out.println(omaKone); }
+		
+		else if (order == 3 && random > 25) {
+			System.out.println("Odota hetki, kaakaotanne valmistetaan");
+			Thread.sleep(1500);
+			omaKone.valmistaKaakao();
+			System.out.println(omaKone); }
+		
+		else if (order == 4 && random > 25) {
+			System.out.println("Sammutetaan");
+			System.out.println("3");
+			Thread.sleep(500);
+			System.out.println("2");
+			Thread.sleep(500);
+			System.out.println("1");
+			Thread.sleep(500);
+			break; }
+		
+		else if (order < 4 && random <= 25) {
+			System.out.println("Kiitos kahvirahoistasi, kone tilttasi. Error code: " + random);
+			Thread.sleep(500);
+			System.out.println("Käynnistetään uudelleen");
+			Thread.sleep(500);
+		}
+		
+		}
+	}
+  }
